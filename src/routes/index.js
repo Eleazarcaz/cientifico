38db463fea0839759c3802a05858c30aabedd6bf
Eleazar Carreon <eleazarcaz@outlook.com>
2020-08-12 21:35:26 -0500

    arreglando bug de hash no detectado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 import Header from '../templates/Header';
 import Character from '../pages/Character';
 import Home from '../pages/Home';
+import Error404 from '../pages/Error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
   '/': Home,
@@ -8,11 +11,15 @@ const routes = {
   '/contact': 'Contact',
 };
 
-const router = async ()=>{
-  const header = null || document.getElementById('header')
+const router = async () => {
+  const header = null || document.getElementById('header');
   const content = null || document.getElementById('content');
 
   header.innerHTML = Header();
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
 };
 
 export default router;
